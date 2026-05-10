e5a962c3f688b3b08b9994460a8528c0e23fe8b6
kkemboi <kemboikelvin185@gmail.com>
2026-05-09 22:15:03 -0400

    Created struc for processes

---
+++ FCFS.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+struct Process{
+    int id;
+    int arrival_time;
+    int completion_time;
+    int turnaround_time;
+    int burst_time;
+    int waiting_time;
+}
+
+int main(){
+    vector<int> processes={{}}
+    return 0;
+}
