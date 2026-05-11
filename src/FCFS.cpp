4f84692507dd80324083191b59191684442c961a
kkemboi <kemboikelvin185@gmail.com>
2026-05-11 01:03:44 -0400

    Refractor: moved FCFS file to src folder

--- src/FCFS.cpp
+++ src/FCFS.cpp
@@ -3,20 +3,21 @@
 #include <algorithm>
 using namespace std;
 
+//Process struct
 struct Process{
     int id;
     int arrival_time;
     int burst_time;
     int turnaround_time=0;
     int waiting_time=0;
-}
+};
 
 int main(){
     //example process objects
     Process p1={1,0,4};
     Process p2={2,2,5};
     Process p3={3,8,2};
-    vector<Process> processes={p1,p2,p3} //vector for Process objects- {id, arrival, burst}
+    vector<Process> processes={p1,p2,p3}; //vector for Process objects- {id, arrival, burst}
     
     //sort by arrival time- ascending order
     sort(processes.begin(), processes.end(), [](const Process& a, const Process& b){
@@ -31,7 +32,7 @@ int main(){
             current_time=p.arrival_time;
         }
         //update waiting, current, and turnaround time
-        p.waiting_time=current_time-arrival_time;
+        p.waiting_time=current_time-p.arrival_time;
         p.turnaround_time=p.waiting_time+p.burst_time;
         current_time+=p.burst_time;
     }
